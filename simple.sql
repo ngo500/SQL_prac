33413adbb44dd7fc2497b06b68f5f3ba0e533565
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-03-18 08:56:59 -0700

    Create simple.sql

---
+++ simple.sql
@@ -0,0 +1,6 @@
+--dataset source:
+--https://data.sfgov.org/Culture-and-Recreation/Film-Locations-in-San-Francisco/yitu-d5am/about_data
+--data set used under Public Domain and Dedication License
+--https://opendatacommons.org/licenses/pddl/1-0/
+
+SELECT * FROM FilmLocations;
