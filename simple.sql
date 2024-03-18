56a1aa5794523b31178f04167945f71100f8375e
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-03-18 09:06:29 -0700

    Update simple.sql

--- simple.sql
+++ simple.sql
@@ -3,4 +3,23 @@
 --data set used under Public Domain and Dedication License
 --https://opendatacommons.org/licenses/pddl/1-0/
 
+-- select everything
 SELECT * FROM FilmLocations;
+
+-- select all film names, director names, writer names
+SELECT Title, Director, Writer FROM FilmLocations;
+
+-- select all film names, release years, and locations
+-- where the release year is 2001 and newer
+SELECT Title, ReleaseYear, Locations FROM FilmLocations WHERE ReleaseYear>=2001;
+
+-- select all fun facts and locations
+SELECT FunFacts, Locations FROM FilmLocations;
+
+-- select all film names, release years, and locations
+-- where the release year is 2000 and older
+SELECT Title, ReleaseYear, Locations FROM FilmLocations WHERE ReleaseYear <= 2000;
+
+-- select all film names, production company names, locations, and release years
+-- where James Cameron was not a writer
+SELECT Title, ProductionCompany, Locations, ReleaseYear FROM FilmLocations WHERE Writer <> "James Cameron";
