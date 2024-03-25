d9123be3f7df6927c2cc2e42a6813b3886892bca
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-03-25 14:19:32 -0700

    Update read_learners.py

--- django_2/read_learners.py
+++ django_2/read_learners.py
@@ -9,3 +9,14 @@ application = get_wsgi_application()
 
 from crud.models import *
 from datetime import date
+
+# Find students with last name "Smith"
+learners_smith = Learner.objects.filter(last_name="Smith")
+print("1. Find learners with last name `Smith`:")
+print(learners_smith)
+print("\n")
+
+# Order by dob descending, and select the first two objects
+learners = Learner.objects.order_by('-dob')[0:2]
+print("2. Find top two youngest learners:")
+print(learners)
