86325d33007f90ec2d9a5bffcd3670af8c0ae587
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-04-01 09:28:57 -0700

    Update read_course_instructors.py

--- django_3/read_course_instructors.py
+++ django_3/read_course_instructors.py
@@ -9,3 +9,34 @@ application = get_wsgi_application()
 
 from related_objects.models import *
 from datetime import date
+
+# forward access
+# Course has instructors reference field so can be used directly via forward access
+courses = Course.objects.filter(instructors__first_name='Yan')
+print("1. Get courses taught by Instructor `Yan`, forward")
+print(courses)
+
+print("\n")
+
+# backward access
+# For each instructor, Django creates a implicit course_set. This is called backward access
+instructor_yan = Instructor.objects.get(first_name='Yan')
+print("1. Get courses taught by Instructor `Yan`, backward")
+print(instructor_yan.course_set.all())
+
+print("\n")
+
+instructors = Instructor.objects.filter(course__name__contains='Cloud')
+print("2. Get the instructors of Cloud app dev course")
+print(instructors)
+
+print("\n")
+
+courses = Course.objects.filter(instructors__first_name='Yan')
+occupation_list = set()
+for course in courses:
+    for learner in course.learners.all():
+        occupation_list.add(learner.occupation)
+
+print("3. Check the occupations of the courses taught by instructor Yan'")
+print(occupation_list)
