f0367f14d6bba40dfb333a1f52e865034395861e
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-04-01 10:59:06 -0700

    Update urls.py

--- django_docker/firstapp/urls.py
+++ django_docker/firstapp/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from . import views
 
+url patterns = [
+    # create a path object that defines the URL pattern to the index view
+    path(route='', view=views.index, name='index'),
+]
