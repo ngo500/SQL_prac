8379c26608732e67072ee1b877c20ef05fdde5e9
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-04-01 11:06:36 -0700

    Update urls.py, date pattern

--- django_docker/firstapp/urls.py
+++ django_docker/firstapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # create a path object that defines the URL pattern to the index view
     path(route='', view=views.index, name='index'),
     # create a path object that defines the URL using '\date' prefix
-    path(route='\date', view=views.get_date, name='date'),
+    path(route='date', view=views.get_date, name='date'),
 ]
 
