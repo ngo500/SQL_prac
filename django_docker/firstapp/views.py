22d34f77fedc5b59b4b5e3d2a36322aa923b0740
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-04-01 11:03:52 -0700

    Update views.py

--- django_docker/firstapp/views.py
+++ django_docker/firstapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
 def index(request):
     # HTML page as a string
@@ -9,3 +10,13 @@ def index(request):
     # return the template as the content argument in HTTP response
     return HttpResponse(content=template)
   
+def get_date(request):
+    # store the date
+    today = date.today()
+    # HTML page with date as a string
+    template =  "<html>" \
+                "Today's date is {}" \
+                "</html>".format(today)
+    # return the template object
+    return HttpResponse(content=template)
+    
