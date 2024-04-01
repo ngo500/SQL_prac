bb0d3d06e81f8b4c8bd0a111ee3bb0cc37cacf50
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-04-01 11:27:09 -0700

    Update admin.py, added InstructorAdmin

--- django_docker/adminsite/admin.py
+++ django_docker/adminsite/admin.py
@@ -4,5 +4,8 @@ from .models import Course, Instructor, Lesson
 class CourseAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'name', 'description']
 
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ['user', 'full_time']
+
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Instructor)
+admin.site.register(Instructor, InstructorAdmin)
