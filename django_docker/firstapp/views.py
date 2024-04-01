31789a8a797c3ccacb967fc70d4a19fbbcbd2211
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-04-01 10:57:42 -0700

    Update views.py

--- django_docker/firstapp/views.py
+++ django_docker/firstapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    # HTML page as a string
+    template =  "<html>" \
+                "This is your first view!" \
+                "</html>"
+    # return the template as the content argument in HTTP response
+    return HttpResponse(content=template)
+  
