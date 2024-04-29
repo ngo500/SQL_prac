2f56908a26e5cbdbdd322018e207efd7060a3455
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-04-29 12:52:58 -0700

    Update views.py

--- django_4/myproject/onlinecourse/views.py
+++ django_4/myproject/onlinecourse/views.py
@@ -29,3 +29,15 @@ def enroll(request, course_id):
         # Return a HTTP response redirecting user to course list view
         return HttpResponseRedirect(reverse(viewname='onlinecourse:popular_course_list'))
         
+def course_details(request, course_id):
+    context = {}
+    if request.method == 'GET':
+        try:
+            course = Course.objects.get(pk=course_id)
+            context['course'] = course
+            # Use render() method to generate HTML page by combining
+            # template and context
+            return render(request, 'onlinecourse/course_detail.html', context)
+        except Course.DoesNotExist:
+            # If course does not exist, throw a Http404 error
+            raise Http404("No course matches the given id.")
