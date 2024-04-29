fef9ba44bdd7dc85e529ce315f3e3e836961a202
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-04-29 12:49:28 -0700

    Update views.py

--- django_4/myproject/onlinecourse/views.py
+++ django_4/myproject/onlinecourse/views.py
@@ -16,3 +16,16 @@ def popular_course_list(request):
         # Append the course list as an entry of context dict
         context['course_list'] = course_list
         return render(request, 'onlinecourse/course_list.html', context)
+
+def enroll(request, course_id):
+    # If request method is POST
+    if request.method == 'POST':
+        # First try to read the course object
+        # If could be found, raise a 404 exception
+        course = get_object_or_404(Course, pk=course_id)
+        # Increase the enrollment by 1
+        course.total_enrollment += 1
+        course.save()
+        # Return a HTTP response redirecting user to course list view
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:popular_course_list'))
+        
