b86992e02ab8e2578489fe96b2c456d9f1686af9
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-05-09 08:52:50 -0700

    Update urls.py

--- django_6/onlinecourse/urls.py
+++ django_6/onlinecourse/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path(route='course/<int:pk>/enroll/', view=views.EnrollView.as_view(), name='enroll'),
     path(route='', view=views.CourseListView.as_view(), name='popular_course_list'),
     path(route='course/<int:pk>/', view=views.CourseDetailsView.as_view(), name='course_details'),
+    # Authentication related urls
+    path('logout/', views.logout_request, name='logout'),
 	
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
