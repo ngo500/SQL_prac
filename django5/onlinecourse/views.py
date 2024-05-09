28a21d1cec096c927cf6fd73307dd24092f3cb94
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-05-09 08:21:58 -0700

    Update views.py

--- django5/onlinecourse/views.py
+++ django5/onlinecourse/views.py
@@ -6,21 +6,36 @@ from django.urls import reverse
 from django.views import generic, View
 from django.http import Http404
 
+# generic class
+# from base generic.ListView class, subclass CourseListView
+class CourseListView(generic.ListView):
+    # get the name of the HTML page for the template
+    template_name = 'onlinecourse/course_list.html'
+    # get the name of the context
+    context_object_name = 'course_list'
+
+    # Override get_queryset() to provide list of objects
+    def get_queryset(self):
+       courses = Course.objects.order_by('-total_enrollment')[:10]
+       # on return, the course list will append into context 'course_list' (given to context_object_name)
+       return courses
+
+# Full logic GET request
 # from base View class, subclass CourseListView
-class CourseListView(View):
-    
+#class CourseListView(View):
+
     # Handles GET request
-    def get(self, request):
-        context = {}
+    #def get(self, request):
+        #context = {}
 
         # Query top 10 popular courses based off total_enrollment
-        course_list = Course.objects.order_by('-total_enrollment')[:10]
+        #course_list = Course.objects.order_by('-total_enrollment')[:10]
 
         # Append list to context
-        context['course_list'] = course_list
+        #context['course_list'] = course_list
 
         # Render page using course_list template and context
-        return render(request, 'onlinecourse/course_list.html', context)
+        #return render(request, 'onlinecourse/course_list.html', context)
 
 # from base View class, subclass EnrollView
 class EnrollView(View):
@@ -37,20 +52,30 @@ class EnrollView(View):
         # Redirect course details page
         return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
 
+# generic class
+# from base generic.DetailView class, subclass CourseDetailsView
+class CourseDetailsView(generic.DetailView):
+    # get the name of the Course for the model
+    model = Course
+    # get the name of the HTML page for the template
+    template_name = 'onlinecourse/course_detail.html'
+    # no return needed
+
+# Full logic GET request
 # from base View class, subclass CourseDetailsView
-class CourseDetailsView(View):
+#class CourseDetailsView(View):
 
     # Handles GET request
-    def get(self, request, *args, **kwargs):
-        context = {}
+    #def get(self, request, *args, **kwargs):
+        #context = {}
         # Get course_id from URL
-        course_id = kwargs.get('pk')
-        try:
+        #course_id = kwargs.get('pk')
+        #try:
             # Get the course object based on course_id
-            course = Course.objects.get(pk=course_id)
+            #course = Course.objects.get(pk=course_id)
             # Append the course object to context
-            context['course'] = course
+            #context['course'] = course
             # Render page using template and course_id
-            return render(request, 'onlinecourse/course_detail.html', context)
-        except Course.DoesNotExist:
-            raise Http404("No course found matching the given id.")
+            #return render(request, 'onlinecourse/course_detail.html', context)
+        #except Course.DoesNotExist:
+            #raise Http404("No course found matching the given id {$course_id}.")
