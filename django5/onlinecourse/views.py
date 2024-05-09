d55ddaf90c7a94988a83585ceee1b6cc4f0232d2
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-05-09 07:56:58 -0700

    Update views.py

--- django5/onlinecourse/views.py
+++ django5/onlinecourse/views.py
@@ -8,6 +8,7 @@ from django.http import Http404
 
 # from base View class, subclass CourseListView
 class CourseListView(View):
+    
     # Handles GET request
     def get(self, request):
         context = {}
@@ -23,18 +24,33 @@ class CourseListView(View):
 
 # from base View class, subclass EnrollView
 class EnrollView(View):
+
     # Handles POST request
     def post(self, request, *args, **kwargs):
         # Get course_id of class being enrolled into
         course_id = kwargs.get('pk')
-
         # Get object of course or 404
         course = get_object_or_404(Course, pk=course_id)
-
         # Increase total enrollment by 1
         course.total_enrollment += 1
         course.save()
-
         # Redirect course details page
-        return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id)))
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
 
+# from base View class, subclass CourseDetailsView
+class CourseDetailsView(View):
+
+    # Handles GET request
+    def get(self, request, *args, **kwargs):
+        context = {}
+        # Get course_id from URL
+        course_id = kwargs.get('pk')
+        try:
+            # Get the course object based on course_id
+            course = Course.objects.get(pk=course_id)
+            # Append the course object to context
+            context['course'] = course
+            # Render page using template and course_id
+            return render(request, 'onlinecourse/course_detail.html', context)
+        except Course.DoesNotExist:
+            raise Http404("No course found matching the given id.")
