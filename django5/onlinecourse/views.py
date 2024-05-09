c607f974188d87f83344cf4101dd875a4ebcbbb0
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-05-09 07:20:40 -0700

    Create views.py

---
+++ django5/onlinecourse/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Course, Lesson, Enrollment
+from django.urls import reverse
+from django.views import generic, View
+from django.http import Http404
+
+# Create your class based views here.
